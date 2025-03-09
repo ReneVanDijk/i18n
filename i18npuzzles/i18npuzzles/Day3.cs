56eb4fae6901fc46dae1b9cfb5427b9fc7d5b7c4
rvd <info@renevandijk.name>
2025-03-09 13:38:47 +0100

    Removed a bit of the clutter

--- i18npuzzles/i18npuzzles/Day3.cs
+++ i18npuzzles/i18npuzzles/Day3.cs
@@ -9,20 +9,14 @@ class Day3 : BaseDay
 
     protected override string Solve(byte[] data)
     {
-        var result = 0;
-
-        var lines = GetLines(data);
-
-        result = lines.Count(LineValid);
-
-        return $"{result}";
-    }
-
-    private static bool LineValid(string line) =>
+        var result = GetLines(data).Count(line =>
                     line.Length >= 4 && line.Length <= 12 &&
                     line.Any(ch => char.IsDigit(ch)) &&
                     line.Any(ch => char.IsUpper(ch)) &&
                     line.Any(ch => char.IsLower(ch)) &&
-                    line.Any(ch => ch > 127);
+                    line.Any(ch => ch > 127));
+
+        return $"{result}";
+    }
 }
 
