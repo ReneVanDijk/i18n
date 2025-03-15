cf7027c33869d4797fac0bc55838a668e6abd174
rvd <info@renevandijk.name>
2025-03-15 13:28:19 +0100

    Small code cleanup for Day 9

--- i18npuzzles/i18npuzzles/Day9.cs
+++ i18npuzzles/i18npuzzles/Day9.cs
@@ -7,6 +7,8 @@ class Day9 : BaseDay
 
     }
 
+    private DateTime NineEleven = new DateTime(2001, 9, 11);
+
     protected override string Solve(byte[] data)
     {
         var lines = GetLines(data);
@@ -19,47 +21,44 @@ class Day9 : BaseDay
             "MM-dd-yy"
         };
 
-        Dictionary<string, List<string>> byName = new();
+        Dictionary<string, List<string>> datesByName = new();
 
         foreach (var line in lines)
         {
-            var numbers = line.Split(":")[0].Split("-");
-
+            var date = line.Split(":")[0];
             var namesOnLine = line.Split(":")[1].Trim();
 
             foreach (var name in namesOnLine.Split(",").Select(s => s.Trim()))
             {
                 var key = name;
 
-                if (byName.ContainsKey(key))
+                if (datesByName.ContainsKey(key))
                 {
-                    byName[key].Add(line.Split(":")[0]);
+                    datesByName[key].Add(date);
                 }
                 else
                 {
-                    byName.Add(key, [line.Split(":")[0]]);
+                    datesByName.Add(key, [date]);
                 }
             }
         }
 
-        List<string> names = new List<string>();
+        List<string> wroteInDiaryOnNineEleven = [];
 
-        foreach (var name in byName)
+        foreach (var (name, dates) in datesByName)
         {
-            var dates = name.Value;
-
             var formatsUsed = formats.Where(format => dates.All(date => DateTime.TryParseExact(date, format, null, System.Globalization.DateTimeStyles.None, out _)));
 
             foreach (var format in formatsUsed)
             {
                 var formattedDates = dates.Select(date => DateTime.ParseExact(date, format, null));
 
-                if (formattedDates.Any(f => f.Year == 2001 && f.Month == 9 && f.Day == 11))
-                    names.Add(name.Key);
+                if (formattedDates.Contains(NineEleven))
+                    wroteInDiaryOnNineEleven.Add(name);
             }
         }
 
-        string result = string.Join(" ", names.OrderBy(s => s).ToArray());
+        string result = string.Join(" ", wroteInDiaryOnNineEleven.OrderBy(s => s).ToArray());
         return $"{result}";
     }
 }
